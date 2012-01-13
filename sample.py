ab26f258c5d06db16454916a2fefe00d522bb5cb
Amauris De Jesus <amauris@peekyou.com>
2012-01-13 16:58:57 -0500

    Updated sample calls

--- sample.py
+++ sample.py
@@ -3,10 +3,12 @@ from peekyou_api import *
 
 test=peekyou_api()
 test.set_key("YOUR API KEY GOES HERE")
+test.set_app_id("YOUR APP ID GOES HERE");
 
 #(Optional)In seconds
 test.set_frequency(1)
 
 # second parameter for get_url can be xml,json
-print test.get_url("http://twitter.com/michaelhussey","json")
+print test.get_social_audience_info("http://twitter.com/michaelhussey","json")
+print test.get_social_consumer_info("http://twitter.com/michaelhussey","json")
 
